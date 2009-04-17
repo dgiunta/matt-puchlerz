6220bdda45c5983a914de84695221ee4030ef834
Matt Puchlerz <matt@puchlerz.com>
2009-04-17 18:49:22 -0500

    Removed the users/show stuff.

--- lib/routes.rb
+++ lib/routes.rb
@@ -6,8 +6,3 @@ get '/stylesheet.css' do
   content_type 'text/css', :charset => 'utf-8'
   sass :stylesheet
 end
-
-get '/users/:name' do |name|
-  @name = name.split('_').map { |word| word.capitalize }.join(' ')
-  haml :'users/show'
-end
