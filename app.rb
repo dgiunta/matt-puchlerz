fdb0791fdf05f11ea78aa7f8f994614784749b81
Matt Puchlerz <matt@puchlerz.com>
2009-04-17 19:54:16 -0500

    Integrated Compass into the mix.

--- app.rb
+++ app.rb
@@ -1,6 +1,30 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
+gem 'chriseppstein-compass', '~> 0.4'
+require 'compass'
 require 'RedCloth'
+
+
+
+#
+# Configuration
+# 
+
+configure do
+  
+  Compass.configuration do |config|
+    config.project_path = File.dirname __FILE__
+    config.sass_dir     = File.join 'views', 'stylesheets'
+  end
+  
+end
+
+
+
+# 
+# Load application files
+# 
 
 def load_or_require(file)
   (Sinatra::Application.environment == :development) ? load(file) : require(file)
--- lib/routes.rb
+++ lib/routes.rb
@@ -2,7 +2,7 @@ get '/' do
   haml :index
 end
 
-get '/stylesheet.css' do
+get '/stylesheets/screen.css' do
   content_type 'text/css', :charset => 'utf-8'
-  sass :stylesheet
+  sass :"stylesheets/screen", :sass => Compass.sass_engine_options
 end
