6d8773b694e93da9f355ee3b7850699fbc728fb4
Matt Puchlerz <matt@puchlerz.com>
2009-04-19 00:26:25 -0500

    Overrode Sinatra::Templates#render_haml to use ' as the attr_wrapper by default." " "

---
+++ lib/sinatra/templates.rb
@@ -0,0 +1,18 @@
+module Sinatra
+  
+  module Templates
+    
+    private        
+    
+    # TODO: Figure out a way to not completely override the method. Tried using 
+    # alias_method but couldn't get rid of the "stack level too deep" error.
+    def render_haml(template, data, options, &block)
+      options[:options] ||= {}
+      options[:options][:attr_wrapper] ||= '"'
+      engine = ::Haml::Engine.new(data, options[:options])
+      engine.render(self, options[:locals] || {}, &block)
+    end
+    
+  end
+  
+end
