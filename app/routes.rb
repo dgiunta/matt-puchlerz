dda18ddddc0c9e71757ecaca3db146552ac6e69e
Matt Puchlerz <matt@puchlerz.com>
2009-04-29 10:28:30 -0500

    Started creating some test works and got them printing to the page.

--- app/routes.rb
+++ app/routes.rb
@@ -1,4 +1,21 @@
 get '/' do
+  @works = []
+  @works << MattPuchlerz::Work.new(
+    :title       => 'Title 1',
+    :description => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    :images      => ['image1.jpg']
+  )
+  @works << MattPuchlerz::Work.new(
+    :title       => 'Title 2',
+    :description => 'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    :images      => ['image2.jpg', 'image2a.jpg']
+  )
+  @works << MattPuchlerz::Work.new(
+    :title       => 'Title 3',
+    :images      => ['image3.jpg', 'image3a.jpg', 'image3b.jpg']
+  )
+  
+  @works = @works.select{ |w| w.viewable? }
   haml :index
 end
 
