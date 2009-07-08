92b546257ce6693f506800a62fe463b1f7a8b0b6
Matt Puchlerz <matt@puchlerz.com>
2009-07-08 12:16:40 -0500

    Got rid of DataMapper require, and all the @works.

--- app.rb
+++ app.rb
@@ -3,8 +3,6 @@ ROOT = File.expand_path File.dirname(__FILE__) unless defined?(ROOT)
 
 require 'rubygems'
 require 'sinatra'
-require 'datamapper'
-# require 'active_support/core_ext/blank'
 
 def load_or_require(file)
   (Sinatra::Application.environment == :development) ? load(file) : require(file)
@@ -46,21 +44,5 @@ end
 
 get '/' do
   @works = []
-  @works << MattPuchlerz::Work.new(
-    :title       => 'Title 1',
-    :description => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    :images      => ['image1.jpg']
-  )
-  # @works << MattPuchlerz::Work.new(
-  #   :title       => 'Title 2',
-  #   :description => 'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  #   :images      => ['image2.jpg', 'image2a.jpg']
-  # )
-  # @works << MattPuchlerz::Work.new(
-  #   :title       => 'Title 3',
-  #   :images      => ['image3.jpg', 'image3a.jpg', 'image3b.jpg']
-  # )
-  # 
-  # @works = @works.select{ |w| w.viewable? }
   haml :index
 end
