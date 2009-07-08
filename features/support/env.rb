30af3b37ce2ae519669fb3aa70b383796c169995
Matt Puchlerz <matt@puchlerz.com>
2009-07-08 11:41:52 -0500

    Fixed and reorganized Cucumber env file.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,16 +1,46 @@
-require 'rubygems'
+ENV['RACK_ENV'] = 'test'
+
+# Sinatra app
+require File.join( File.dirname(__FILE__), *%w[ .. .. app ] )
+
+# RSpec matchers
 require 'spec/expectations'
+
+
+
+# 
+# Webrat configuration
+# 
+
 require 'webrat'
-require File.join( File.dirname(__FILE__), '..', 'support', 'paths' )
-require File.join( File.dirname(__FILE__), '..', '..', 'boot' )
+require File.join( File.dirname(__FILE__), *%w[ . paths ] )
+
+module Webrat
+  class Session
+    # Overriding to make 400+ response codes unsuccessful
+    def success_code?
+      (200..399).include?(response_code)
+    end
+  end
+end
 
 Webrat.configure do |config|
   config.mode = :sinatra
 end
+
+
+
+# 
+# Cucumber configuration
+# 
 
 World do
   session = Webrat::SinatraSession.new
   session.extend(Webrat::Matchers)
   session.extend(Webrat::HaveTagMatcher)
   session
+end
+
+Before do
+  DataMapper.auto_migrate!
 end
