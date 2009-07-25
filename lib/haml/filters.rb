ed95b57606bc9a1ccd928c50cd71a7d76b1efa13
Matt Puchlerz <matt@puchlerz.com>
2009-07-25 16:41:52 -0500

    Fixed the TextileInline issue with the new version of Haml. Not sure why I needed to be more specific, but whatev.

--- lib/haml/filters.rb
+++ lib/haml/filters.rb
@@ -4,10 +4,10 @@ module Haml
   module Filters
     
     module TextileInline
-      include Base
+      include Haml::Filters::Base
       
       def render(text)
-        textiled = Textile.render(text)
+        textiled = Haml::Filters::Textile.render(text)
         textiled = textiled[3..-1] if textiled[0..2] == "<p>"
         textiled = textiled[0..-5] if textiled[-4..-1] == "</p>"
         textiled
