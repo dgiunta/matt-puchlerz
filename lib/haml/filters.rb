d680b2f4872c46f3fae4539af035910c8517fb70
Matt Puchlerz <matt@puchlerz.com>
2009-07-26 20:55:43 -0500

    Fixed the textileinline Haml filter.

--- lib/haml/filters.rb
+++ lib/haml/filters.rb
@@ -4,10 +4,11 @@ module Haml
   module Filters
     
     module TextileInline
-      include Haml::Filters::Base
+      include Base
+      lazy_require 'redcloth'
       
       def render(text)
-        textiled = Haml::Filters::Textile.render(text)
+        textiled = Textile.render(text)
         textiled = textiled[3..-1] if textiled[0..2] == "<p>"
         textiled = textiled[0..-5] if textiled[-4..-1] == "</p>"
         textiled
