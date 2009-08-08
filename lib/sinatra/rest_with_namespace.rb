05827204c168087ec66cdfeba41a5cde419a2dcf
Matt Puchlerz <matt@puchlerz.com>
2009-08-07 23:36:46 -0500

    Added namespace + underscore to view files. Now more organized within the views. Fixed up a few things.

--- lib/sinatra/rest_with_namespace.rb
+++ lib/sinatra/rest_with_namespace.rb
@@ -11,7 +11,10 @@ module Sinatra
     
     def read_config(filename)
       super
-      @config.each_value { |hash| hash[:url] = @namespace + hash[:url] } if @namespace
+      @config.each_value do |route| 
+        route[:render].sub! 'PLURAL/', "PLURAL/#{ @namespace }_"
+        route[:url] = @namespace + route[:url] 
+      end if @namespace
     end
     
   end
