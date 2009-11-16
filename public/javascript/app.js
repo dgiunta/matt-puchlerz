ca5825a32e00a89f920bbd6c8ddb9c97899429e4
Matt Puchlerz <matt@puchlerz.com>
2009-11-15 21:40:43 -0600

    Forgot to get rid of the Cufon initializing script.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,13 +1,5 @@
 window.addEvent('domready', function() { 
              
-  [ 'strong', '#document_footer p' ].each( function(selector) {
-    $$(selector).each( function(el) {
-      var text = el.get('text');
-      text = text.replace('–', '-');
-      text = text.replace('—', '--');
-      el.set('text', text);          
-    });
-    Cufon.replace(selector);
-  });
+	
     
 });
